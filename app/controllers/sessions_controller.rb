ed09cc8e8ed73bd736afbae7a0c55a4d0ef55922
rodrigo santos <rodrigov12.santos@gmail.com>
2025-03-18 20:53:28 -0400

    criacao da pagina de login e redirecionamento da pagina junto ao botao de login na pagina incial

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,13 @@
 class SessionsController < ApplicationController
-  # Ação para exibir o formulário de login
+  layout "login"  # Usando um layout específico para a tela de login
+
   def new
   end
 
-  # Ação para criar uma sessão (login)
   def create
     @user = User.find_by(email: params[:email])
 
-    # Verifica se o usuário existe e se a senha está correta
     if @user&.authenticate(params[:password])
-      # Inicia a sessão do usuário
       session[:user_id] = @user.id
       redirect_to @user, notice: 'Login realizado com sucesso.'
     else
@@ -18,7 +16,6 @@ class SessionsController < ApplicationController
     end
   end
 
-  # Ação para destruir a sessão (logout)
   def destroy
     session[:user_id] = nil
     redirect_to root_path, notice: 'Desconectado com sucesso.'
